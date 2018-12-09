4562873fdf498779b6c515ce22e99478152238f1
Spoorthi S Madhavan <36541492+spoorthimadhavan@users.noreply.github.com>
2018-12-09 12:13:06 +0530

    RestaurantRepository  CategoryRepository

--- Course5_Project/Exercise-master/src/main/java/org/upgrad/repositories/CategoryRepository.java
+++ Course5_Project/Exercise-master/src/main/java/org/upgrad/repositories/CategoryRepository.java
@@ -1,33 +1,25 @@
 package org.upgrad.repositories;
 
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.upgrad.models.Category;
-
+import org.springframework.transaction.annotation.Transactional;
+import org.upgrad.models.Category; 
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Set;
 
-
 @Repository
 public interface CategoryRepository extends CrudRepository<Category,Integer> {
 
-    @Transactional
-    @Modifying
-    @Query(nativeQuery = true, value = "insert into category (title,description) values (?1,?2)")
-    void addCategoryValues(String title,String description);
-
-    @Query(nativeQuery = true, value="select * from category where id=?1")
-    Category getCategory(int categoryId);
-
     @Query(nativeQuery = true, value="select * from category")
-    List<Category> getAllCategories();
-
-    @Query(nativeQuery = true, value="select * from category where id=?1")
-    Long checkCategory(int categoryId);
+    List<Category> getAllCategory();
 
 
+
+    @Query(nativeQuery = true,value="select * from question where user_id=?1")
+    List<Category> getAllCategoryByCategoryName(String categoryName);
 }
-
